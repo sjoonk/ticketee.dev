2f998b625ad5ef662639cfb5de65c8091468fd5c
Sukjoon Kim <sjoonk@gmail.com>
2013-07-12 01:16:37 +0900

    Added updating projects functionality

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ProjectsTest < ActionDispatch::IntegrationTest
-  fixtures :all
+  # fixtures :all
 
   feature "Creating projects" do
     
@@ -26,6 +26,28 @@ class ProjectsTest < ActionDispatch::IntegrationTest
       page.must_have_content "Project has not been created."
       page.must_have_content "Name can't be blank"
     end
+
+  end
+
+
+  feature "Updating projects" do
+
+    # In order to update project information
+    # As as user
+    # I want to be able to do that through an interface
+    
+    it "update a project" do
+      @project = FactoryGirl.create(:project, :name => "TextMate2")
+      visit root_path
+      click_on "TextMate2"
+      click_on "Edit Project"
+      fill_in "Name", :with => "TextMate2 beta"
+      click_on "Update Project"
+      page.must_have_content "Project has been updated."
+      current_path.must_equal project_path(@project)
+    end
+    
+    scenario "Updating with invalid attributes"
 
   end
 
